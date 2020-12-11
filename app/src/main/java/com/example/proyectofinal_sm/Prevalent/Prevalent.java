8a963840dc2347e2f1772db10178c23f9c12700c
Edwarto <71796703+Edwarto@users.noreply.github.com>
2020-12-10 20:00:35 -0500

    proyecto

---
+++ app/src/main/java/com/example/proyectofinal_sm/Prevalent/Prevalent.java
@@ -0,0 +1,11 @@
+package com.example.proyectofinal_sm.Prevalent;
+
+import com.example.proyectofinal_sm.Model.Users;
+
+public class Prevalent
+{
+    public static Users currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
---
+++ app/src/main/java/com/example/proyectofinal_sm/ui/home/HomeFragment.java
@@ -0,0 +1,53 @@
+package com.example.proyectofinal_sm.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.proyectofinal_sm.Model.Products;
+import com.example.proyectofinal_sm.R;
+import com.example.proyectofinal_sm.ViewHolder.ProductViewHolder;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
+
+public class HomeFragment extends Fragment {
+
+    private HomeViewModel homeViewModel;
+
+    private RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
+    private DatabaseReference ProductsRef;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        homeViewModel =
+                new ViewModelProvider(this).get(HomeViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        //final TextView textView = root.findViewById(R.id.text_home);
+        /*homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });*/
+
+
+
+        return root;
+
+    }
+
+}
